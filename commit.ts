0d8006d131f5c1fb244b241cdf2862e579b157a3
Tim Hutt <tdhutt@gmail.com>
2024-10-23 18:22:42 +0100

    Inherit stdout and fix exception messages

--- commit.ts
+++ commit.ts
@@ -2,23 +2,23 @@
 
 async function main() {
     const diff = await new Deno.Command("git", { args: ["status", "--porcelain"] }).output();
-    if (diff.code !== 0) {
+    if (!diff.success) {
         throw Error("git diff failed");
     }
     if (diff.stdout.byteLength !== 0) {
         console.log("Changes detected - committing & pushing.");
         // Changes detected.
-        const add_status = await new Deno.Command("git", { args: ["--no-pager", "add", "."] }).output();
-        if (add_status.code !== 0) {
+        const add_status = await new Deno.Command("git", { args: ["--no-pager", "add", "."] }).spawn().status;
+        if (!add_status.success) {
             throw Error("git add failed");
         }
-        const commit_status = await new Deno.Command("git", { args: ["--no-pager", "commit", "-m", "Automatic Update"] }).output();
-        if (commit_status.code !== 0) {
-            throw Error("git add failed");
+        const commit_status = await new Deno.Command("git", { args: ["--no-pager", "commit", "-m", "Automatic Update"] }).spawn().status;
+        if (!commit_status.success) {
+            throw Error("git commit failed");
         }
-        const push_status = await new Deno.Command("git", { args: ["--no-pager", "push"] }).output();
-        if (push_status.code !== 0) {
-            throw Error("git add failed");
+        const push_status = await new Deno.Command("git", { args: ["--no-pager", "push"] }).spawn().status;
+        if (!push_status.success) {
+            throw Error("git push failed");
         }
     } else {
         console.log("No changes detected.");
